f4d1a59e88d1bdb07c1368eea8ffcd2a25f2f9fa
swarna <swarnasras@yahoo.com>
2015-02-16 23:42:11 +0530

    project1.1 from coursera

---
+++ data explore/Plot1.R
@@ -0,0 +1,24 @@
+library("data.table")
+
+#read file
+file=fread("household_power_consumption.txt")
+file1=as.data.frame(file)
+rm(file)
+#checks if date is 1st or 2nd feb 2007
+data=file1[file1$Date %in% c("1/2/2007","2/2/2007"),]
+rm(file1)
+#converts data types
+data$Date=as.Date(data$Date,format="%d/%m/%Y")
+data$Global_active_power=as.numeric(data$Global_active_power)
+data$Global_reactive_power=as.numeric(data$Global_reactive_power)
+data$Voltage=as.numeric(data$Voltage)
+data$Global_intensity=as.numeric(data$Global_intensity)
+data$Sub_metering_1=as.numeric(data$Sub_metering_1)
+data$Sub_metering_2=as.numeric(data$Sub_metering_2)
+data$Sub_metering_3=as.numeric(data$Sub_metering_3)
+
+#plots the chart
+png(filename="plot1.png", width=504, height=504)
+par(bg = "transparent")
+hist(data$Global_active_power, col="red",xlab="Global Active Power (kilowatts)",ylab="Frequency",main="Global Active Power")
+dev.off()
