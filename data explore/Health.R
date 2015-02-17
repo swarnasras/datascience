64dd384a8f72e3771a99fdfe582af0a7ef72a527
swarna <swarnasras@yahoo.com>
2015-02-17 11:57:43 +0530

    New update on health source. added image

--- data explore/Health.R
+++ data explore/Health.R
@@ -1,4 +1,19 @@
+a=read.csv("healthdata.csv")
+a$Height=as.character(a$Height)
+a$Height=as.numeric(a$Height)
+a$DPI=as.character(a$DPI)
+a$DPI=as.numeric(a$DPI)
+b=a[a$Gender=="F",]
+c=a[a$Gender=="M",]
+d=rbind(b,c)
+d$DPIQuantile=cut(d$DPI,breaks=quantile(d$DPI,na.rm=T))
+d$HeightQuantile=cut(d$Height,breaks=quantile(d$Height,na.rm=T))
+#a$HeightQuantile=cut(d$Height, breaks=c(120,130,140,150,160,170))
+
 qplot(Height,Weight,data=a, color=Gender, facets=.~Batch,geoms=c("point","smooth"), method="lm")
+qplot(Height,DPI,data=d, color=Gender, facets=.~HeightQuantile,geoms=c("point","smooth"), method="lm")
+qplot(Height,DPI,data=d, color=Gender, facets=.~Batch, method="lm") +geom_smooth()
+qplot(Height,data=d)
 subset(a,a$Gender=="M")
 power$Date <- as.Date(power$Date, format="%d/%m/%Y")
 two.days <- all.data[all.data$Date == "1/2/2007" | all.data$Date == "2/2/2007",]
